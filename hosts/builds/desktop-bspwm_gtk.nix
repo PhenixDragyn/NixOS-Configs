e96d3276c5f24871bdaa17fa03ab82bca7d87120
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-08-11 20:46:00 -0600

    Addes some autorandr stuff

--- hosts/builds/desktop-bspwm_gtk.nix
+++ hosts/builds/desktop-bspwm_gtk.nix
@@ -25,7 +25,31 @@
     #displayManager.sddm.enable = true;
   };
 
-  services.autorandr.enable = true;
+  services.autorandr = {
+    enable = true;
+    profiles = {
+      "Virtual" = {
+        fingerprint = {
+          Virtual-1 = "00ffffffffffff0049143412000000002a180104a520147806ee91a3544c99260f5054210800e1c0d1c0d100a940b300950081808140ea2900c051201c304026444045cb10000018000000f7000a004082002820000000000000000000fd00327d1ea0ff010a202020202020000000fc0051454d55204d6f6e69746f720a013a02030b00467d6560591f6100000010000000000000000000000000000000000010000000000000000000000000000000000010000000000000000000000000000000000010000000000000000000000000000000000010000000000000000000000000000000000010000000000000000000000000000000000000000000002f";
+        };
+        config = {
+          Virtual-1 = {
+            enable = true;
+            crtc = 0;
+            primary = true;
+            position = "0x0";
+            mode = "1920x1200";
+            #gamma = "1.0:0.909:0.833";
+            rate = "59.88";
+            #rotate = "left";
+          };
+        };
+      };
+    };
+  };
+
+  #services.udev.packages = [pkgs.autorandr];
+  #services.udev.extraRules = ''ACTION=="change", SUBSYSTEM=="drm", RUN+="${pkgs.autorandr}/bin/autorandr -c"'';
 
   # Enable security services
   services.gnome.gnome-keyring.enable = true;
