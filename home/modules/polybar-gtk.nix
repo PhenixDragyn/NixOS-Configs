6041d6e93f1249c501fae22c9ffb9257043e3f41
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-08-12 21:02:17 -0600

    Added some rofi

--- home/modules/polybar-gtk.nix
+++ home/modules/polybar-gtk.nix
@@ -692,7 +692,7 @@ fi
           format = "%{T2} %{T-}";
           format-foreground = "\${colors.orange}";
           format-padding=1;
-          click-left = "~/.config/polybar/scripts/Power/power-gtk.sh";
+          click-left = "~/.config/rofi/powermenu/power-gtk.sh";
         };
 
         "module/powermenu2" = {
