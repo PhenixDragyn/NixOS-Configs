1a5979ae7f05c853659114f3055e1a7d988a148c
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-08-15 11:14:20 -0600

    Updates

--- hosts/builds/common.nix
+++ hosts/builds/common.nix
@@ -264,13 +264,13 @@
 
   # ---------------------------------
 
-  #environment.etc."current-system-packages".text =
-  #  let
-  #    packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
-  #    sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
-  #    formatted = builtins.concatStringsSep "\n" sortedUnique;
-  #  in
-  #    formatted;
+  environment.etc."current-system-packages".text =
+    let
+      packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
+      sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
+      formatted = builtins.concatStringsSep "\n" sortedUnique;
+    in
+      formatted;
 
   #system.stateVersion = "24.05";
 }
