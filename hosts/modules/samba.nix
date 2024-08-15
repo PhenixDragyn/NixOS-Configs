1baf239a8a5a6db731a06c89b52ed0e11a44219f
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-08-15 09:24:17 -0600

    Updates

--- hosts/modules/samba.nix
+++ hosts/modules/samba.nix
@@ -89,41 +89,6 @@
   #networking.firewall.allowedTCPPorts = [ 445 139 ];
   #networking.firewall.allowedUDPPorts = [ 137 138 ];
 
-
-  #services.avahi = {
-  #  publish.enable = true;
-  #  publish.userServices = true;
-  #  nssmdns4 = true;
-  #  enable = true;
-  #  #openFirewall = true;
-  #};
-
-  #services.avahi = {
-  #  enable = true;
-  #  nssmdns = true;
-  #  publish = {
-  #    enable = true;
-  #    addresses = true;
-  #    domain = true;
-  #    hinfo = true;
-  #    userServices = true;
-  #    workstation = true;
-  #  };
-  #  extraServiceFiles = {
-  #    smb = ''
-  #      <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
-  #      <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-  #      <service-group>
-  #        <name replace-wildcards="yes">%h</name>
-  #        <service>
-  #          <type>_smb._tcp</type>
-  #          <port>445</port>
-  #        </service>
-  #      </service-group>
-  #    '';
-  #  };
-  #};
-
   services.samba-wsdd = {
     enable = true;
     openFirewall = true;
