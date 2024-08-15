58a5d6b8188d2baf0ae07ad4f332c60eb97994f3
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-08-15 08:20:47 -0600

    Updates

--- flake.nix
+++ flake.nix
@@ -53,8 +53,7 @@
       username = "ejvend";
       name = "Ejvend";
       email = "ejvend.nielsen@gmail.com";
-      theme = "shapeshifter";
-      #theme = "ia-dark";
+      theme = "ia-dark";
       #theme = "helios";
       # To view sample themes..  https://tinted-themeing.github.io/base16-gallery
     }; 
