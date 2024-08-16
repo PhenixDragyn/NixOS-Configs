666726fe9d0e65d1b6ad0e930b4cc57850f771f7
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-08-16 13:39:52 -0600

    Updates

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
   let
     inherit (self) outputs;
     
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     stateVersion = "24.05";
 
     # host = osConfig.networking.hostname;
