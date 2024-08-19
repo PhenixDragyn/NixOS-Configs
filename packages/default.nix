08d065c9f1088f88aea31b06859a64dd2b2ab762
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-08-19 10:34:47 -0600

    Updates

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,7 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-{ pkgs, ... }: {
-#{pkgs ? import <nixpkgs> {}, ...}: rec {
+#{ pkgs, ... }: {
+{pkgs ? import <nixpkgs> {}, ...}: rec {
   # example = pkgs.callPackage ./example { };
   qrsync = pkgs.callPackage ./qrsync { };
 }
