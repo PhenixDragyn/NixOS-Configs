93255d664fbc7675e18fb1ff2c1c6f5481c59d54
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-09-03 12:02:48 -0600

    Updates

--- hosts/nixos-lt/configuration.nix
+++ hosts/nixos-lt/configuration.nix
@@ -53,7 +53,8 @@
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
-# You can also add overlays exported from other flakes:
+
+      # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
 
       # Or define it inline, for example:
