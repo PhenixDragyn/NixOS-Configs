50e499a3a13f4270338b24cd0f93f13295240739
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-09-04 07:19:26 -0600

    Updates

--- hosts/nixos-lt/configuration.nix
+++ hosts/nixos-lt/configuration.nix
@@ -2,10 +2,11 @@
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 { inputs, outputs, lib, config, pkgs, stable, unstable, buildSettings, stateVersion, ... }: 
 
-{ # You can import other NixOS modules here
+{ 
+  # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos): outputs.nixosModules.example
-# Or modules from other flakes (such as nixos-hardware):
+    # Or modules from other flakes (such as nixos-hardware):
     # inputs.hardware.nixosModules.common-cpu-amd
     # inputs.hardware.nixosModules.common-ssd
     inputs.nixvim.nixosModules.nixvim
