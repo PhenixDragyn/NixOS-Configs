a9b3e2c07c5c9210e8ce9d8aad057e64d9705053
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-09-08 17:30:45 -0600

    Update

--- home/common/modules/nixvim.nix
+++ home/common/modules/nixvim.nix
@@ -61,6 +61,9 @@
         enable = true;
         keymaps = {
           "<leader>ff" = "find_files";
+          "<leader>fg" = "live_grep";
+          "<leader>fb" = "buffers";
+          "<leader>fh" = "help_tags";
         };
       };
 
