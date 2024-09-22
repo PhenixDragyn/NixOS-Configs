76489d424c253d0d1d271c887055033c924d7a9b
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-09-22 14:27:46 -0600

    Update

---
+++ home/common/desktops/hypr_lxqt/default.nix
@@ -0,0 +1,41 @@
+#{ inputs, outputs, lib, config, pkgs, stable, unstable, username, hostname, platform, build, theme, isWorkstation, stateVersion, ... }:
+{ config, lib, pkgs, username, ... }:
+
+{
+  imports = [
+    # Modules
+    ../../modules/fastfetch.nix
+    ../../modules/ranger.nix
+    ../../modules/nixvim.nix 
+
+    ../../modules/firefox.nix
+    ../../modules/kitty.nix
+    ../../modules/nautilus.nix
+
+    ../../modules/gtk.nix
+    ../../modules/qt.nix
+
+    ../../modules/dunst.nix
+    ../../modules/hyprland.nix
+    ../../modules/rofi-wayland.nix
+		#../../modules/swaylock.nix
+		#../../modules/swayosd.nix
+    ../../modules/waybar.nix
+    ../../modules/wlogout.nix
+
+		../../modules/imv.nix
+
+		../../modules/hyprlock.nix
+		#../../modules/hyprpaper.nix
+  ];
+
+  # ---------------------------------
+
+  # SYMLINKS
+  home = {
+    file.".config/autostart".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/NixOS/home/users/${username}/config/autostart";
+    #file.".config/xfce".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/NixOS/home/${username}/config/xfce";
+
+    file.".config/syncthingtray.ini".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/NixOS/home/users/${username}/config/syncthing/syncthingtray.ini";
+  };
+}
