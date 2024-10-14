9bad892c7a3126e50ea012229b805983e23633d5
PhenixDragyn <ejvend.nielsen@gmail.com>
2024-10-13 19:25:09 -0600

    Update

--- stylix/stylix.nix
+++ stylix/stylix.nix
@@ -20,6 +20,15 @@
       size = 24;
     };
 
+    # fonts = {
+    #   serif = config.stylix.fonts.monospace;
+    #   sansSerif = config.stylix.fonts.monospace;
+    #   monospace = {
+    #     package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+    #     name = "JetBrainsMono Nerd Font";
+    #   };
+    # };
+
     fonts = {
       sansSerif = {
         package = pkgs.dejavu_fonts;
